1d6e22b57e765fb6d88c8552b6443a36b1c8a0a4
gusrb8925 <gusrb8925@naver.com>
2020-05-08 11:20:42 +0900

    신규 프로젝트 올립니다.

--- JavaPrj/src/Hello.java
+++ JavaPrj/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¾È³ç!");
+		System.out.println("¾È³ç! °­Çö±Ô");
 	}
 
 }
