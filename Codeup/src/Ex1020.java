0338f4c03f1bf79d8c6f47d3b17e7b3461e8a068
gusrb8925 <gusrb8925@naver.com>
2020-05-11 20:57:24 +0900

    코드업 연습

---
+++ Codeup/src/Ex1020.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex1020 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		String[] ar = a.split("\\-");
+		System.out.printf("%s%s", ar[0], ar[1]);
+	}
+
+}
