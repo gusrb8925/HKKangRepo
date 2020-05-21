c151d3cc7c8971aaf2a4a3aa99fbae357b4ad0e6
gusrb8925 <gusrb8925@naver.com>
2020-05-21 14:12:30 +0900

    프로그래머스 자바 lv1 수박

---
+++ Programmers/src/Watermelon.java
@@ -0,0 +1,27 @@
+
+public class Watermelon {
+	public static String solution(int n) {
+		String answer = "";
+		String su = "¼ö";
+		String bak = "¹Ú";
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				answer += su;
+			} else {
+				answer += bak;
+			}
+
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int i = 3;
+		int j = 4;
+		System.out.println(solution(i));
+		System.out.println(solution(j));
+
+	}
+
+}
