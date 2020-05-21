1c82c4ffa1e63087e4e0080691f1b3ef9d66e134
gusrb8925 <gusrb8925@naver.com>
2020-05-21 14:49:05 +0900

    프로그래머스 자바 lv1 문자열을 정수로 바꾸기

---
+++ Programmers/src/Strchangeint.java
@@ -0,0 +1,15 @@
+
+public class Strchangeint {
+	public static int solution(String s) {
+		int answer = Integer.parseInt(s);
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String str1 = "1234";
+		String str2 = "-1234";
+		System.out.println(solution(str1));
+		System.out.println(solution(str2));
+	}
+
+}
