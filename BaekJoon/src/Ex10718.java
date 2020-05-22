d8bdb17aba6a1d7cbcc8419e617aec090d06501f
gusrb8925 <gusrb8925@naver.com>
2020-05-22 11:31:48 +0900

    백준

--- BaekJoon/src/Ex10718.java
+++ BaekJoon/src/Ex10718.java
@@ -2,8 +2,8 @@
 public class Ex10718 {
 
 	public static void main(String[] args) {
-		System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
-		System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
+		System.out.println("ê°•í•œì¹œêµ¬ ëŒ€í•œìœ¡êµ°");
+		System.out.println("ê°•í•œì¹œêµ¬ ëŒ€í•œìœ¡êµ°");
 
 	}
 
