39684aee95ea359758308ac0051964c642ca4511
gusrb8925 <gusrb8925@naver.com>
2020-05-22 11:31:20 +0900

    프로그래머스 자바 lv1 완주하지 못한 선수

--- Programmers/src/Marathon.java
+++ Programmers/src/Marathon.java
@@ -5,15 +5,15 @@ public class Marathon {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		String start = s.nextLine(); //참가자 입력받기
-		String finish = s.nextLine();// 완주자 입력받기
+		String start = s.nextLine(); 
+		String finish = s.nextLine();
 		
-		String[] ar = start.split("\\,");//참가자 배열
-		String[] br = finish.split("\\,");//완주자 배열 
+		String[] ar = start.split("\\,");
+		String[] br = finish.split("\\,");
 		
 		for(int i=0;i<ar.length;i++) {
             for (int j=0;j<br.length;j++){
-                if (ar[i].equals(br[j])) { //참가자와 완주자 같은이름이면 null처리
+                if (ar[i].equals(br[j])) { 
                     ar[i] = "";
                     br[j] = "";
                     break;
@@ -22,7 +22,7 @@ public class Marathon {
         }
 		
 		for (int i=0; i<ar.length;i++) {
-			if(ar[i] != "") { //null 값이 아닌 완주자만 출력한다.
+			if(ar[i] != "") { 
 				System.out.print(ar[i]+" ");
 			}
 		}
