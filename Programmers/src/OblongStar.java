b8fa73a7a90479a7b7dbf8e5911e942732e3bcf3
Kkyu <gusrb8925@naver.com>
2020-05-31 00:12:18 +0900

    프로그래머스 자바 lv1 직사각형 별찍기

--- Programmers/src/OblongStar.java
+++ Programmers/src/OblongStar.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class OblongStar {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
@@ -13,5 +12,4 @@ public class OblongStar {
 			System.out.println("");
 		}
 	}
-
 }
