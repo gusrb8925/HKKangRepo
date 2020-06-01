09b9aecc1bfdb7faf8c09766d98ee5cf7fb3348b
Kkyu <gusrb8925@naver.com>
2020-06-01 21:44:19 +0900

    백준

---
+++ BaekJoon/src/Ex2884.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex2884 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int h = s.nextInt();
+		int m = s.nextInt();
+
+		m -= 45;
+		if (m < 0) {
+			if (h != 0) {
+				h -= 1;
+				m += 60;
+			} else {
+				h = 23;
+				m += 60;
+			}
+		}
+		System.out.println(h + " " + m);
+	}
+
+}
