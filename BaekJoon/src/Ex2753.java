d4e49d57ea2464b4e1886d1150a24f0d567eb5ea
Kkyu <gusrb8925@naver.com>
2020-06-01 21:43:47 +0900

    백준

---
+++ BaekJoon/src/Ex2753.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex2753 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) {
+			System.out.println("1");
+		} else
+			System.out.println("0");
+	}
+
+}
