dd9419b685f1e962756bff30fd7be24e6f76ae11
Kkyu <gusrb8925@naver.com>
2020-06-01 09:02:16 +0900

    프로그래머스 자바 lv1 가운데 글자 가져오기

--- Programmers/src/BringcenterString.java
+++ Programmers/src/BringcenterString.java
@@ -4,13 +4,13 @@ class BringcenterString {
 		if (a.length() % 2 == 0) {
 			return a.substring(a.length() / 2 - 1, a.length() / 2 + 1);
 		} else {
-			return Character.toString(a.charAt(a.length() / 2));
+			return String.valueOf(a.charAt(a.length() / 2));
 		}
 	}
 
 	public static void main(String[] args) {
 		BringcenterString s = new BringcenterString();
-		System.out.println(s.solution("center"));
-		System.out.println(s.solution("power"));
+		System.out.println(s.solution("abcde"));
+		System.out.println(s.solution("qwer"));
 	}
 }
