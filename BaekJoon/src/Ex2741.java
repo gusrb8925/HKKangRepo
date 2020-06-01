dca3e409ca6cae0ce4b080715d956a47995fe2b3
Kkyu <gusrb8925@naver.com>
2020-06-01 21:43:26 +0900

    백준

---
+++ BaekJoon/src/Ex2741.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex2741 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		for (int i=1; i<=a;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
