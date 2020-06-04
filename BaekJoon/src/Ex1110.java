1283b4a4099e67b8ff9e08e3ec5ec892a9c38858
Kkyu <gusrb8925@naver.com>
2020-06-04 09:01:25 +0900

    백준

---
+++ BaekJoon/src/Ex1110.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex1110 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if (a < 10) {
+			a *= 10;
+		}
+		int one = a / 10;
+		int two = a % 10;
+		int save;
+		int count = 0;
+		while (true) {
+			count++;
+			save = two;
+			two = (one + two) % 10;
+			one = save;
+			if (a == ((one * 10) + two)) {
+				break;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
