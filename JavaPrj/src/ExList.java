d4a99f34d7e459b2783eac98f7f73f7f79aed3dc
Kkyu <gusrb8925@naver.com>
2020-06-06 11:32:40 +0900

    자바

---
+++ JavaPrj/src/ExList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExList {
+
+	public static void main(String[] args) {
+		List<String> al = new ArrayList<>();
+
+		al.add("이협건");
+		al.add("홍길동");
+		al.add("임꺽정");
+		al.add("고길동");
+
+		System.out.println("배열의 크기" + al.size());
+
+		for (int i = 0; i < al.size(); i++) {
+			System.out.println("[" + i + "]번째 배열 기억공간 :" + al.get(i));
+		}
+
+		Iterator<String> it = al.iterator();
+		while (it.hasNext()) {
+			System.out.println("이름 : " + it.next());
+		}
+
+	}
+
+}
