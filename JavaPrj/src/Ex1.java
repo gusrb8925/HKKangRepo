b30a5ef74f8a0a24301b2bbe93f49280f6c38726
Kkyu <gusrb8925@naver.com>
2020-06-06 11:32:09 +0900

    자바

---
+++ JavaPrj/src/Ex1.java
@@ -0,0 +1,17 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		for (int i = 3; i <= 8; i++) {
+			if (i == 3 || i == 5) {
+				System.out.println("<" + i + "단>");
+			}
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + "*" + j + "=" + (i * j));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
