3a44d220311fa02410c6b7fae672a28066a55698
kkyu <gusrb8925@naver.com>
2020-06-15 10:02:17 +0900

    백준

---
+++ BaekJoon/src/Ex18108.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex18108 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println(a - 543);
+
+	}
+
+}
