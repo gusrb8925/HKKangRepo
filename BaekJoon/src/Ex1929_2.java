e38c84c4f1c536d51a0ca5b085c3d2f48842824a
kkyu <gusrb8925@naver.com>
2020-06-16 13:38:53 +0900

    백준

---
+++ BaekJoon/src/Ex1929_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex1929_2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int[] ar = new int[b + 1];
+
+		for (int i = 0; i < b + 1; i++) {
+			ar[i] = 0;
+		}
+		ar[1] = 1;
+		for (int i = 2; i <= b; i++) {
+			for (int j = 2; i * j <= b; j++) {
+				ar[i * j] = 1;
+			}
+		}
+		for (int i = a; i <= b; i++) {
+			if (ar[i] != 1) {
+				System.out.println(i);
+			}
+		}
+	}
+}
