6bb332d744aa6b20c334f07b5ec91d5ed0d9edff
kkyu <gusrb8925@naver.com>
2020-06-18 08:56:47 +0900

    백준

---
+++ BaekJoon/src/Ex1085.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex1085 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int y = s.nextInt();
+		int w = s.nextInt();
+		int h = s.nextInt();
+
+		System.out.println(Math.min(Math.min(w - x, x), Math.min(h - y, y)));
+	}
+
+}
