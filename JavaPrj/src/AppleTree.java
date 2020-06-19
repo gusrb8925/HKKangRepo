d3632c58e02b77ee183334f7a796ba9aad140863
kkyu <gusrb8925@naver.com>
2020-06-19 12:43:32 +0900

    자바

--- JavaPrj/src/AppleTree.java
+++ JavaPrj/src/AppleTree.java
@@ -3,4 +3,14 @@ public class AppleTree extends Tree {
 	public void exe() {
 		super.treePrint();
 	}
+
+	@Override
+	public void treePrint() {
+		System.out.println("수퍼클래스에서 정의한 함수를 이것으로 변경함.");
+	}
+
+	public static void main(String[] args) {
+		AppleTree at = new AppleTree();
+		at.treePrint();
+	}
 }
