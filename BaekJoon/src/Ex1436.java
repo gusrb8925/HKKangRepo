be7879401718eadc55c2a327348b35eb22024d1e
kkyu <gusrb8925@naver.com>
2020-06-23 08:54:59 +0900

    백준

---
+++ BaekJoon/src/Ex1436.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex1436 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int count = 0;
+		while (n > 0) {
+			count++;
+			if (String.valueOf(count).contains("666")) {
+				n--;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
