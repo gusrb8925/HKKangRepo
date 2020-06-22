9bcd06ff30b949717ca8879d5dd45ea47c087111
kkyu <gusrb8925@naver.com>
2020-06-23 08:54:47 +0900

    백준

---
+++ BaekJoon/src/Ex1427.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex1427 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		String[] ar = str.split("");
+		Arrays.sort(ar, Collections.reverseOrder());
+
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i]);
+		}
+
+	}
+
+}
