c0532ff16f5454e3d6e460df4df750663188ed90
kkyu <gusrb8925@naver.com>
2020-06-24 08:58:38 +0900

    백준

---
+++ BaekJoon/src/Ex1550.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex1550 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		System.out.println(Integer.valueOf(a, 16));
+
+	}
+
+}
