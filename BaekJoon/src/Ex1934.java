7b95e0a806cbb13200633d73abd88896bd73b8a7
kkyu <gusrb8925@naver.com>
2020-06-24 09:00:47 +0900

    백준

---
+++ BaekJoon/src/Ex1934.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex1934 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			System.out.println(a * b / gcd(a, b));
+		}
+	}
+
+	public static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		} else {
+			return gcd(b, a % b);
+		}
+
+	}
+
+}
