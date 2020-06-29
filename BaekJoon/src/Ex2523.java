111e85cce42fd4f678b19104d690a8252fd927b1
kkyu <gusrb8925@naver.com>
2020-06-29 11:55:02 +0900

    백준

---
+++ BaekJoon/src/Ex2523.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex2523 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < a - 1; i++) {
+			for (int j = a - 1 - i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
