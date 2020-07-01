76a95d995252e3cd76a805f1cd11832b1c26b602
kkyu <gusrb8925@naver.com>
2020-07-01 12:15:01 +0900

    백준

---
+++ BaekJoon/src/Ex2562.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Ex2562 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int[] arr = new int[9];
+		for (int i = 0; i < 9; i++) {
+			arr[i] = s.nextInt();
+		}
+		int max = arr[0];
+		for (int i = 0; i < 9; i++)
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+
+		for (int i = 0; i < 9; i++) {
+			if (max == arr[i]) {
+				System.out.println(arr[i]);
+				System.out.println(i + 1);
+			}
+		}
+	}
+
+}
