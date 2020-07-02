77878c1963ba190fcb36b01a722646af5e6d219f
kkyu <gusrb8925@naver.com>
2020-07-02 09:28:29 +0900

    백준

---
+++ BaekJoon/src/Ex2446.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex2446 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (a * 2 - 1) - (2 * i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < a - 1; i++) {
+			for (int j = 0; j < a - 2 - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 3 + (2 * i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
