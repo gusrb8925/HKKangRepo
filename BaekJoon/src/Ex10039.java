8eab622c49fa1dd8b6a06ad3a49c70ead61c4a1f
kkyu <gusrb8925@naver.com>
2020-07-06 18:45:00 +0900

    백준 10039번 평균 점수

---
+++ BaekJoon/src/Ex10039.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex10039 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int a = s.nextInt();
+			if (a >= 40) {
+				sum += a;
+			} else {
+				sum += 40;
+			}
+		}
+
+		System.out.println(sum / 5);
+
+	}
+
+}
