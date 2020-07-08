73a8c57254362c98bbe36a1c8bc38d29c45d317d
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-09 00:35:22 +0900

    백준 자바 2743번 - 단어 길이 재기

---
+++ BaekJoon/src/Ex2743.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex2743 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		System.out.println(str.length());
+
+	}
+
+}
