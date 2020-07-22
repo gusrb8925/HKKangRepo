143af895ecbb466b117d7a51d9c702b831414534
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-22 12:36:05 +0900

    백준 자바 1094번 - 막대기

--- BaekJoon/src/Ex1094.java
+++ BaekJoon/src/Ex1094.java
@@ -4,9 +4,12 @@ public class Ex1094 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int len = 64;
 		int x = s.nextInt();
+		s.close();
+
+		int len = 64;
 		int count = 0;
+
 		while (x > 0) {
 			if (len > x) {
 				len /= 2;
