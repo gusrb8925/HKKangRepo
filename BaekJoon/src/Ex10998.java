91b79c7e9c468e9f664d6ea3ac0f8958d903ddff
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-22 12:40:50 +0900

    백준 자바 10998번 - A×B

--- BaekJoon/src/Ex10998.java
+++ BaekJoon/src/Ex10998.java
@@ -4,9 +4,11 @@ public class Ex10998 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		s.close();
+
 		int a = s.nextInt();
 		int b = s.nextInt();
-		System.out.println(a*b);
+		System.out.println(a * b);
 	}
 
 }
