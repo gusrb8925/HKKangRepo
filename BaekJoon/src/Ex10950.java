56544c45f268de09489b96ae03b84c0243de71cd
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-22 12:39:57 +0900

    백준 자바 10950번 - A+B - 3

--- BaekJoon/src/Ex10950.java
+++ BaekJoon/src/Ex10950.java
@@ -5,6 +5,8 @@ public class Ex10950 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		for (int i = 0; i < a; i++) {
 			int b = s.nextInt();
 			int c = s.nextInt();
