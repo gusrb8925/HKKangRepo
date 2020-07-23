5d7a628e766bce34f0b87d1d112386bf0e477b63
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:06:28 +0900

    백준 자바 2440번 - 별 찍기 - 3

--- BaekJoon/src/Ex2440.java
+++ BaekJoon/src/Ex2440.java
@@ -5,13 +5,15 @@ public class Ex2440 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		for (int i = 0; i < a; i++) {
-			for (int j = i; j <a; j++) {
+			for (int j = i; j < a; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
