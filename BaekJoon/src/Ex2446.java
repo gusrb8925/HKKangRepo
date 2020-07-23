7431abe11a005bc735676b3c647a6abca63dd0c9
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:07:50 +0900

    백준 자바 2446번 - 별 찍기 - 9

--- BaekJoon/src/Ex2446.java
+++ BaekJoon/src/Ex2446.java
@@ -4,6 +4,7 @@ public class Ex2446 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
 
 		for (int i = 0; i < a; i++) {
 			for (int j = 0; j < i; j++) {
