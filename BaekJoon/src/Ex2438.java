73a9732a6442f9970cdbdbe5bc5e0f8015c6f99f
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:05:57 +0900

    백준 자바 2438번 - 별 찍기 - 1

--- BaekJoon/src/Ex2438.java
+++ BaekJoon/src/Ex2438.java
@@ -5,6 +5,8 @@ public class Ex2438 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		for (int i = 0; i < a; i++) {
 			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
