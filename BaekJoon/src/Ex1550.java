f76097a5e5722414b91f7f8732d178796b95b0ad
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:03:54 +0900

    백준 자바 1550번 - 16진수

--- BaekJoon/src/Ex1550.java
+++ BaekJoon/src/Ex1550.java
@@ -5,6 +5,8 @@ public class Ex1550 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String a = s.next();
+		s.close();
+
 		System.out.println(Integer.valueOf(a, 16));
 
 	}
