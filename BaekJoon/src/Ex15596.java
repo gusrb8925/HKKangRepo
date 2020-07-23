f5d0f59d9a79065202abe984f244ed7520a3a496
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:11:18 +0900

    백준 자바 15596번 - 정수 N개의 합

--- BaekJoon/src/Ex15596.java
+++ BaekJoon/src/Ex15596.java
@@ -1,9 +1,9 @@
 public class Ex15596 {
-    long sum(int[] a) {
-        long sum = 0;
-        for(int i  = 0; i < a.length; i++){
-            sum += a[i];
-        }
-        return sum;
-    }
+	long sum(int[] a) {
+		long sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
 }
