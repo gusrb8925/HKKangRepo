a6d62828137fa9bd3e7ff6148050f107fee35c5b
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:13:03 +0900

    백준 자바 16170번 - 오늘의 날짜는?

--- BaekJoon/src/Ex16170.java
+++ BaekJoon/src/Ex16170.java
@@ -3,17 +3,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Ex16170 {
-    public static void main(String[] args) throws IOException {
-        Date today = new Date();
+	public static void main(String[] args) throws IOException {
+		Date today = new Date();
 
-        SimpleDateFormat year = new SimpleDateFormat("yyyy");
-        SimpleDateFormat month = new SimpleDateFormat("MM");
-        SimpleDateFormat day = new SimpleDateFormat("dd");
+		SimpleDateFormat year = new SimpleDateFormat("yyyy");
+		SimpleDateFormat month = new SimpleDateFormat("MM");
+		SimpleDateFormat day = new SimpleDateFormat("dd");
 
-        System.out.println(year.format(today));
-        System.out.println(month.format(today));
-        System.out.println(day.format(today));
+		System.out.println(year.format(today));
+		System.out.println(month.format(today));
+		System.out.println(day.format(today));
 
-
-    }
+	}
 }
