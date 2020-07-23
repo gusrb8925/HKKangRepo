e9cacb9b836696f5db6bc875fea4760efa09be14
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-23 13:04:54 +0900

    백준 자바 1934번 - 최소공배수

--- BaekJoon/src/Ex1934.java
+++ BaekJoon/src/Ex1934.java
@@ -5,6 +5,8 @@ public class Ex1934 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
+		s.close();
+
 		for (int i = 0; i < n; i++) {
 			int a = s.nextInt();
 			int b = s.nextInt();
