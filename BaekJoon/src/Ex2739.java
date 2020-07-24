8f55df347c158677f4e5249b6bcafc01751a97f3
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-24 12:48:37 +0900

    백준 자바 2739번 - 구구단

--- BaekJoon/src/Ex2739.java
+++ BaekJoon/src/Ex2739.java
@@ -5,6 +5,8 @@ public class Ex2739 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		for (int i = 1; i < 10; i++) {
 			System.out.println(a + " * " + i + " = " + (a * i));
 		}
