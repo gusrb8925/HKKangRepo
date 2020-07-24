a9699e17202573d1a5766e7cc2828916bb743de0
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-24 12:46:34 +0900

    백준 자바 2556번 - 별 찍기 - 14

--- BaekJoon/src/Ex2556.java
+++ BaekJoon/src/Ex2556.java
@@ -4,6 +4,7 @@ public class Ex2556 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		sc.close();
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
