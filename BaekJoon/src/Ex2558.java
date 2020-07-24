c6fe533f46e92a4991590cf7f351405a40b844af
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-24 12:47:03 +0900

    백준 자바 2558번 - A+B - 2

--- BaekJoon/src/Ex2558.java
+++ BaekJoon/src/Ex2558.java
@@ -4,6 +4,8 @@ public class Ex2558 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		s.close();
+
 		int a = s.nextInt();
 		int b = s.nextInt();
 		System.out.println(a + b);
