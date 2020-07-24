e152bb7eaf69f7b4c89c0f82df715e6a6439d3d7
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-24 12:47:18 +0900

    백준 자바 2562번 - 최댓값

--- BaekJoon/src/Ex2562.java
+++ BaekJoon/src/Ex2562.java
@@ -4,10 +4,13 @@ public class Ex2562 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+
 		int[] arr = new int[9];
 		for (int i = 0; i < 9; i++) {
 			arr[i] = s.nextInt();
 		}
+		s.close();
+
 		int max = arr[0];
 		for (int i = 0; i < 9; i++)
 			if (max < arr[i]) {
