5d43ca8d9bfb4ae6d88e9d57134220645dfc8ab3
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-24 12:45:41 +0900

    백준 자바 2522번 - 별 찍기 - 12

--- BaekJoon/src/Ex2522.java
+++ BaekJoon/src/Ex2522.java
@@ -5,6 +5,8 @@ public class Ex2522 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		for (int i = 0; i < a; i++) {
 			for (int j = 0; j < a - 1 - i; j++) {
 				System.out.print(" ");
