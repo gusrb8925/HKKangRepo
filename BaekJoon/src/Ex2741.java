612d1c785d09c67e37b4e9a0c2fba43b3d07134f
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-28 00:06:48 +0900

    백준 자바 2741번 - N 찍기

--- BaekJoon/src/Ex2741.java
+++ BaekJoon/src/Ex2741.java
@@ -5,7 +5,9 @@ public class Ex2741 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
-		for (int i=1; i<=a;i++) {
+		s.close();
+
+		for (int i = 1; i <= a; i++) {
 			System.out.println(i);
 		}
 
