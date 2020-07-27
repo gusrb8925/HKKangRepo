8cf7f8c0d80c3748e273c957272db0898a7b3106
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-28 00:08:31 +0900

    백준 자바 2753번 - 윤년

--- BaekJoon/src/Ex2753.java
+++ BaekJoon/src/Ex2753.java
@@ -5,6 +5,8 @@ public class Ex2753 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+		s.close();
+
 		if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) {
 			System.out.println("1");
 		} else
