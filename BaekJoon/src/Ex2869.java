fe8fe93bc52d02bf568b48960862cb85a1d54d5f
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-07-28 00:09:07 +0900

    백준 자바 2869번 - 달팽이는 올라가고 싶다

--- BaekJoon/src/Ex2869.java
+++ BaekJoon/src/Ex2869.java
@@ -7,6 +7,8 @@ public class Ex2869 {
 		int a = s.nextInt();
 		int b = s.nextInt();
 		int v = s.nextInt();
+		s.close();
+
 		int day = 0;
 		day = (v - b) / (a - b);
 		if ((v - b) % (a - b) != 0) {
