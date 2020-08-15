1d6af29ccade7e3dbf0e5a2dc2b0dc9d78d9c4f1
kkyu <gusrb8925@naver.com>
2020-08-15 16:15:21 +0900

    백준 자바 1037번 - 약수

---
+++ BaekJoonSilver/src/Ex1037.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex1037 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int[] arr = new int[a];
+		for (int i = 0; i < a; i++) {
+			arr[i] = s.nextInt();
+		}
+		s.close();
+
+		Arrays.sort(arr);
+		System.out.println(arr[0] * arr[a - 1]);
+
+	}
+
+}
