28145e7132219425ca9d7164251a8cf0ef02bf8e
kkyu <gusrb8925@naver.com>
2020-08-15 16:15:42 +0900

    백준 자바 1057번 - 토너먼트

---
+++ BaekJoonSilver/src/Ex1057.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex1057 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		s.close();
+		int count = 0;
+
+		while (b != c) {
+			b = (b + 1) / 2;
+			c = (c + 1) / 2;
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
