de02221d36ac9d6402c9cc12a7af4386c643e23b
kkyu <gusrb8925@naver.com>
2020-08-15 16:15:00 +0900

    백준 자바 1008번 - A/B

---
+++ BaekJoonBronze/src/Ex1008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex1008 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double a = s.nextDouble();
+		double b = s.nextDouble();
+		System.out.println(a / b);
+
+	}
+
+}
