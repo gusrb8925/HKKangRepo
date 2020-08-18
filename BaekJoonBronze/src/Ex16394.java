242b79863e7acfcac09e51029c84c461ee168c63
kkyu <gusrb8925@naver.com>
2020-08-18 17:02:40 +0900

    백준 자바 16394번 - 홍익대학교

---
+++ BaekJoonBronze/src/Ex16394.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex16394 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		System.out.println(a - 1946);
+
+	}
+
+}
