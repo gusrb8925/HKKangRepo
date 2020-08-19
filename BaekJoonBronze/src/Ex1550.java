ad387e1f2f0bad60a24bf82f20d770fe87ff2e14
kkyu <gusrb8925@naver.com>
2020-08-19 09:01:10 +0900

    백준 자바 1550번 - 16진수

---
+++ BaekJoonBronze/src/Ex1550.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex1550 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		s.close();
+
+		System.out.println(Integer.valueOf(a, 16));
+
+	}
+
+}
