8c6bc035b82924a39a9a76bd5141e556e1faf0e1
kkyu <gusrb8925@naver.com>
2020-08-22 22:29:40 +0900

    백준 자바 2440번 - 별 찍기 - 3

---
+++ BaekJoonBronze/src/Ex2440.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex2440 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		for (int i = 0; i < a; i++) {
+			for (int j = i; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
