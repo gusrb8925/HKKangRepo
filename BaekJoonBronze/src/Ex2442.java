57832fb8c0e8f962e96a01aef4de44d8a8516e81
kkyu <gusrb8925@naver.com>
2020-08-23 16:51:14 +0900

    백준 자바 2442번 - 별 찍기 - 5

---
+++ BaekJoonBronze/src/Ex2442.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex2442 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		for (int i = 1; i < a + 1; i++) {
+			for (int j = 0; j < a - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < 2 * i - 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
