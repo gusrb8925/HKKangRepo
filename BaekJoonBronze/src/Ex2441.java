1a73e417ba49eea6e824d435a66529810f7536ec
kkyu <gusrb8925@naver.com>
2020-08-23 16:50:59 +0900

    백준 자바 2441번 - 별 찍기 - 4

---
+++ BaekJoonBronze/src/Ex2441.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex2441 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < a; j++) {
+				if (i > j) {
+					System.out.print(" ");
+				} else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
