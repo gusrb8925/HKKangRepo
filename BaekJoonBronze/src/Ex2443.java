549c11a35442b316c7f3cd8847a28931b9e3348a
kkyu <gusrb8925@naver.com>
2020-08-23 16:51:29 +0900

    백준 자바 2443번 - 별 찍기 - 6

---
+++ BaekJoonBronze/src/Ex2443.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex2443 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		for (int i = 0; i < a; i++) {
+			for (int k = 0; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = a * 2 - 1; j > i * 2; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
