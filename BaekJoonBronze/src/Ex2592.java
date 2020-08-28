2af49f704c8960a75496245231246ea4fedaa804
kkyu <gusrb8925@naver.com>
2020-08-28 10:19:43 +0900

    자바 백준 2592번 - 대표값

---
+++ BaekJoonBronze/src/Ex2592.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex2592 {
+
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int arr[] = new int[1001];
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			int a = s.nextInt();
+			sum += a;
+			arr[a]++;
+		}
+		int max = 0;
+		int maxIndex = 0;
+		for (int i = 0; i < 1001; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+				maxIndex = i;
+			}
+		}
+		s.close();
+		System.out.println(sum / 10);
+		System.out.println(maxIndex);
+
+	}
+}
