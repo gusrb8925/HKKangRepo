66698cbadbcc96367e19e8ae84113ce5235ba6cc
kkyu <gusrb8925@naver.com>
2020-08-28 10:18:15 +0900

    자바 백준 2558번 - A+B - 2

---
+++ BaekJoonBronze/src/Ex2558.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex2558 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		s.close();
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.println(a + b);
+	}
+
+}
