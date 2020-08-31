e77db250d786b3d3a267519bf53a8caa96dfb72b
kkyu <gusrb8925@naver.com>
2020-08-31 15:23:46 +0900

    백준 자바 2742번 - 기찍 N

---
+++ BaekJoonBronze/src/Ex2742.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex2742 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		s.close();
+
+		for (int i = a; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
