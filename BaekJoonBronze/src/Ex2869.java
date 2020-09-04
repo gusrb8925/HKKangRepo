f2c73bcea93830048e3dd48763e1e450a769be33
kkyu <gusrb8925@naver.com>
2020-09-04 12:46:51 +0900

    백준 자바 2869번 - 달팽이는 올라가고 싶다

---
+++ BaekJoonBronze/src/Ex2869.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex2869 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int v = s.nextInt();
+		s.close();
+
+		int day = 0;
+		day = (v - b) / (a - b);
+		if ((v - b) % (a - b) != 0) {
+			day++;
+		}
+		System.out.println(day);
+	}
+
+}
