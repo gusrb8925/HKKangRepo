6106275f4bf5a56645f73370fa82c68495f7eada
gusrb8925 <gusrb8925@naver.com>
2020-09-13 13:53:06 +0900

    백준 자바 3046번 - R2

---
+++ BaekJoonBronze/src/Ex3046.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex3046 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int r1 = s.nextInt();
+		int ss = s.nextInt();
+		System.out.println((ss * 2) - r1);
+
+	}
+
+}
