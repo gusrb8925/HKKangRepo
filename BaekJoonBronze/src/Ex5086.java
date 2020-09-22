a6526975bd700c2263be8c9cf06279417b8e9977
gusrb8925 <gusrb8925@naver.com>
2020-09-22 10:30:40 +0900

    백준 자바 5086번 - 배수와 약수

---
+++ BaekJoonBronze/src/Ex5086.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex5086 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		while (true) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			if (a == 0 && b == 0)
+				break;
+
+			if (b % a == 0) {
+				System.out.println("factor");
+			} else if (a % b == 0) {
+				System.out.println("multiple");
+			} else {
+				System.out.println("neither");
+			}
+		}
+	}
+
+}
