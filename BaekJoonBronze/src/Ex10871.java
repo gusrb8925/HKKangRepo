c0083df361d49390bd26815fbc0a93b91bf6a991
gusrb8925 <gusrb8925@naver.com>
2020-10-11 00:25:13 +0900

    백준 자바 10871번 - X보다 작은 수

---
+++ BaekJoonBronze/src/Ex10871.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex10871 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int x = s.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = s.nextInt();
+			if (a < x) {
+				System.out.println(a);
+			}
+		}
+
+	}
+
+}
