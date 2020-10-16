0fe93d8fcf6ffc73f00eed9441c76115b1d57959
gusrb8925 <gusrb8925@naver.com>
2020-10-16 13:38:56 +0900

    백준 자바 10952번 - A+B - 5

---
+++ BaekJoonBronze/src/Ex10952.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex10952 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		while (true) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			if (a == 0 && b == 0) {
+				break;
+			}
+			System.out.println(a + b);
+
+		}
+	}
+
+}
