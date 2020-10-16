38dda1e56e1e21cc8bef7572686724b4a26cc3d1
gusrb8925 <gusrb8925@naver.com>
2020-10-16 13:37:37 +0900

    백준 자바 10926번 - ??!

---
+++ BaekJoonBronze/src/Ex10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex10926 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String id = s.next();
+
+		System.out.println(id + "??!");
+	}
+
+}
