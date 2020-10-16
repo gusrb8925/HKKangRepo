54918507c46170d493046891f0b98a5dea24ec7f
gusrb8925 <gusrb8925@naver.com>
2020-10-16 13:37:54 +0900

    백준 자바 10943번 - 랜덤 게임~

---
+++ BaekJoonBronze/src/Ex10943.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Random;
+
+public class Ex10943 {
+	public static void main(String[] args) throws IOException {
+		Random rand = new Random();
+		System.out.println(((rand.nextInt(10) + 1)));
+
+	}
+
+}
