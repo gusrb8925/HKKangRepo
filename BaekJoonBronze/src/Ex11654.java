8a427f564afaeeed3c74a754ae154e024e241b5b
gusrb8925 <gusrb8925@naver.com>
2020-10-18 15:08:46 +0900

    백준 자바 11654번 - 아스키 코드

---
+++ BaekJoonBronze/src/Ex11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex11654 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		int b = a.charAt(0);
+		System.out.println(b);
+	}
+
+}
