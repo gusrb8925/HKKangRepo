b0e29585407cef2330c19d813cb7857e46cbc38f
gusrb8925 <gusrb8925@naver.com>
2020-10-18 15:07:51 +0900

    백준 자바 11382번 - 꼬마 정민

---
+++ BaekJoonBronze/src/Ex11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex11382 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long a = s.nextLong();
+		long b = s.nextLong();
+		long c = s.nextLong();
+		System.out.println(a + b + c);
+	}
+
+}
