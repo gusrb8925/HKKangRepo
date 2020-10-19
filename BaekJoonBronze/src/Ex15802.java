91424f0456ce368178e2fa9bf64093d74f68e848
gusrb8925 <gusrb8925@naver.com>
2020-10-19 15:57:26 +0900

    백준 자바 15802번 - 타노스

---
+++ BaekJoonBronze/src/Ex15802.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex15802 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String name = br.readLine();
+
+		if (name.equals("kkyu8925")) {
+			bw.write(String.valueOf("1"));
+		} else {
+			bw.write(String.valueOf("0"));
+		}
+
+		bw.flush();
+
+	}
+
+}
