69b01ea26ea6220c22c1495681f1f5db10827072
gusrb8925 <gusrb8925@naver.com>
2020-12-11 10:47:52 +0900

    백준 자바 15680번 - 연세대학교

---
+++ BaekJoonBronze/src/Ex15680.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex15680 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if (a == 0) {
+			System.out.println("YONSEI");
+		} else if (a == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+
+	}
+
+}
