19da72cd44a5da12da7a0ecffcae4db4a218683e
kkyu <gusrb8925@naver.com>
2021-04-24 13:12:58 +0900

    21-04-24 listnode

---
+++ ThinkDataStructures/src/main/java/com/allendowney/thinkdast/LinkedListExample.java
@@ -0,0 +1,23 @@
+package com.allendowney.thinkdast;
+
+public class LinkedListExample {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+
+        ListNode node0 = new ListNode(0, node1);
+        System.out.println(node0);
+        System.out.println(node1);
+        System.out.println(node2);
+        System.out.println(node3);
+    }
+}
