71e807bbe138c359f62df15924c08823fdf7d607
kkyu <gusrb8925@naver.com>
2021-05-01 12:20:55 +0900

    21-05-01 SillyString

---
+++ ThinkDataStructures/src/main/java/com/allendowney/thinkdast/SillyString.java
@@ -0,0 +1,43 @@
+package com.allendowney.thinkdast;
+
+import java.util.Map;
+
+public class SillyString {
+    private final String innerString;
+
+    public SillyString(String innerString) {
+        this.innerString = innerString;
+    }
+
+    public String toString() {
+        return innerString;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this.toString().equals(other.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        int total = 0;
+        for (int i = 0; i < innerString.length(); i++) {
+            total += innerString.charAt(i);
+        }
+        System.out.println(total);
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Map<SillyString, Integer> map = new MyBetterMap<>();
+
+        map.put(new SillyString("Word1"), 1);
+        map.put(new SillyString("Word2"), 2);
+        Integer value = map.get(new SillyString("Word1"));
+        System.out.println(value);
+
+        for (SillyString key : map.keySet()) {
+            System.out.println(key + ", " + map.get(key));
+        }
+    }
+}
