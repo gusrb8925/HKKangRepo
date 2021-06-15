140026421d8796a8733181ad2390baa5be65053c
kkyu <gusrb8925@naver.com>
2021-06-15 22:28:23 +0900

    21-06-15 StringBuilder reverse

--- JavaCodingTest/src/section01/ex07_회문_문자열/Main.java
+++ JavaCodingTest/src/section01/ex07_회문_문자열/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public String solution(String str) {
         String answer = "YES";
         int len = str.length();
-
         str = str.toUpperCase();
         for (int i = 0; i < len / 2; i++) {
             if (str.charAt(i) != str.charAt(len - 1 - i)) {
