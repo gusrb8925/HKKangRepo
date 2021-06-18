4c67b3e1a060bfd759299cc98ae1877517296ca6
kkyu <gusrb8925@naver.com>
2021-06-18 13:28:26 +0900

    21-06-18 two pointer

--- JavaCodingTest/src/section03/ex05_연속된_자연수의_합/Main.java
+++ JavaCodingTest/src/section03/ex05_연속된_자연수의_합/Main.java
@@ -1,4 +1,39 @@
 package section03.ex05_연속된_자연수의_합;
 
+import java.util.Scanner;
+
 public class Main {
+    private int solution(int n) {
+        int answer = 0;
+
+        int m = n / 2 + 1;
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = i + 1;
+        }
+
+        int lt = 0;
+        int sum = 0;
+        for (int rt = 0; rt < m; rt++) {
+            sum += arr[rt];
+            if (sum == n) {
+                answer++;
+            }
+
+            while (sum >= n) {
+                sum -= arr[lt++];
+                if (sum == n) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
+    }
 }
