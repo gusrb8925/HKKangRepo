bd68ffc24725d3ca58cb7f07a9a6712ed814f12b
kkyu <gusrb8925@naver.com>
2021-06-24 13:26:50 +0900

    21-06-15 array

--- JavaCodingTest/src/section02/ex04_피보나치_수열/Main.java
+++ JavaCodingTest/src/section02/ex04_피보나치_수열/Main.java
@@ -13,6 +13,17 @@ public class Main {
         return answer;
     }
 
+    private void solution2(int n) {
+        int a = 1, b = 1, c;
+        System.out.println(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.println(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
